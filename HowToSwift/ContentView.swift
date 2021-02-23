0dc98e925a393034abc8a278531059db755ce605
Moi Gutierrez <moyoteg@gmail.com>
2021-02-22 18:54:55 -0600

    add all files

--- HowToSwift/ContentView.swift
+++ HowToSwift/ContentView.swift
@@ -10,35 +10,53 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-
+    
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+        ZStack {
+            Color.blue
+            
+            NavigationView {
+                
+                Group {
+                    if items.isEmpty {
+                        Text("no items")
+                    } else {
+                        List {
+                            ForEach(items) { item in
+                                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                            }
+                            .onDelete(perform: deleteItems)
+                        }
+                    }
+                }
+                .toolbar {
+                    HStack {
+                        #if os(iOS)
+                        EditButton()
+                        #endif
+                        
+                        Spacer()
+                        
+                        Button(action: addItem) {
+                            Label("Add Item", systemImage: "plus")
+                        }
+                    }
+                }
             }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
+            
         }
     }
-
+    
     private func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
-
+            
             do {
                 try viewContext.save()
             } catch {
@@ -49,11 +67,11 @@ struct ContentView: View {
             }
         }
     }
-
+    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach(viewContext.delete)
-
+            
             do {
                 try viewContext.save()
             } catch {
